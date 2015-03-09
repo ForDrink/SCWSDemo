c50f6b3e4911ddd776b5602ad3205683cd8b62ee
ForDrink <ForDrink@yangfujundeMacBook-Pro.local>
2015-03-09 15:03:01 +0800

    initial commit

--- SCWSDemo/ViewController.m
+++ SCWSDemo/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "AFNetworking.h"
 
 //
 //
@@ -27,5 +28,24 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+- (IBAction)goButtonTapped:(id)sender {
+    NSLog(@"goButtonTapped");
+    
+    
+    AFHTTPSessionManager *sessionManager = [AFHTTPSessionManager manager];
+    
+//    NSDictionary *params = @{@"data" : self.textField.text, @"respond" : @"json"};
+    
+    [sessionManager POST:@"http://api.yi18.net/drug/list"
+              parameters:nil
+                 success:^(NSURLSessionDataTask *task, id responseObject) {
+                     NSLog(@"responseObject:%@", responseObject);
+                 } failure:^(NSURLSessionDataTask *task, NSError *error) {
+                     NSLog(@"error:%@", error);
+                 }];
+}
+
 
 @end
